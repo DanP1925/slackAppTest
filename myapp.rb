d758e2a8f7272a2213081d93d78e5557049919be
Daniel <DanP1925@gmail.com>
2020-03-21 23:55:38 -0500

    First try, it displays messages

---
+++ myapp.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# myapp.rb
+require 'sinatra'
+require 'slack-ruby-client'
+require 'dotenv/load'
+Dotenv.load
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+  raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+end
+
+client = Slack::Web::Client.new
+
+client.auth_test
+all_members = client.users_list
+all_members['members'].each do |member|
+  if member.is_bot != true
+    client.chat_postMessage(channel: member.id, text: 'Habla causita',
+                            as_user: true)
+  end
+end
