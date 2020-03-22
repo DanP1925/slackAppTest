3fd4ca53f721e3da036c98a1a5a51ac48afa495d
Daniel <DanP1925@gmail.com>
2020-03-22 15:42:54 -0500

    Test with slack client

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ require 'slack-ruby-client'
 require 'dotenv/load'
 Dotenv.load
 
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+  raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+end
+
+client = Slack::Web::Client.new
+
 get '/' do
-  'Hello World!'
+  client.auth_test
 end
